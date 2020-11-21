17c0bb4b609db54df5223c958e07d5c87c77f48f
akaSakka <badiesakka@gmail.com>
2020-11-21 23:20:20 +0100

    word count+ trim

--- algo_programing/ch1_records_files/string_manipulation/Trim.pas
+++ algo_programing/ch1_records_files/string_manipulation/Trim.pas
@@ -39,13 +39,28 @@ Begin
     fnTrim:=tmpString;
 End;
 
+Function fnWordCount(vString:String):Integer;
+Var
+    Words,i:Integer;
+Begin
+	Words:=0;
+	For i:=1 to Length(vString) Do
+	Begin
+		If vString[i]=#32 Then 
+            Words:=Words+1;
+    End;
+    fnWordCount:=Words;    
+End;
+
 (*Main Program*)
 Begin
     str1:='         Ali va       a l''ecole  .  Le temps fait beau      .';
     str2:='Ali va       a l''ecole  .  Le temps fait beau      .  ';
     Writeln('Str1= ',Str1);
     WriteLn('Str1 Trimmed=',fnTrim(str1));
+    Writeln('Number of words in str1:',fnWordCount(str1));
     WriteLn('Str2= ',Str2);
     WriteLn('Str2 Trimmed=',fnTrim(str2));
+   WriteLn('Number of words in str2:',fnWordCount(str2));
     Readkey;
 End.
